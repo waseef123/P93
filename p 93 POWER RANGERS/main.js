81858d29fad0d3c8cc75516801e8e70b96d9abdb
crossmark <120784648+waseef123@users.noreply.github.com>
2023-05-11 16:08:18 +0400

    Add files via upload

---
+++ p 93 POWER RANGERS/main.js
@@ -0,0 +1,63 @@
+var canvas = new fabric.Canvas("myCanvas") 
+
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img) {
+		block_image_object = Img ;
+		block_image_object.scaleToWidth(block_image_width)
+		block_image_object.scaleToHeight(block_image_height)
+	block_image_object.set({top: block_y, left:block_x}) ;
+	canvas.add(block_image_object)
+	}) ;
+
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		block_x = 10;
+		new_image('rr1.png');
+		console.log ('R')
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 310;
+		new_image('gr.png');
+		console.log ('G')
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =560;
+		new_image('yr.png');
+		console.log ('Y')
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 880;
+		new_image('pr.png');
+		console.log ('P')
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 900;
+		new_image('br.png');
+		console.log ('B')
+	}
+	
+}
+
